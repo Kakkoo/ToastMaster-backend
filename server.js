e7fa45cdf82875552bd4824c0fff76c7070f2df8
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-09-11 11:15:34 -0700

    some

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const users = require('./routes/api/users');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const main = require("./routes/api/main");
 const record = require("./routes/api/record");
 const app = express();
 
 //Body parser configuration
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//First route
+app.get("/", (req, res) => res.send("Hello World"));
 
-app.use('/api/users', users);
-app.use('/api/main', main);
-app.use('/api/record', record);
+//Use routes
+app.use("/api/users", users);
+app.use("/api/main", main);
+app.use("/api/record", record);
 
 const port = 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-//DB congig
-const db = require('./config/keys').mongoURI;
+
+//Db config
+const db = require("./config/keys").mongoURI;
 //Connect to mongodb
 mongoose
-.connect(db)
-.then(() => console.log('MongoDb connected'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDb Connected"))
+  .catch((err) => console.log(err));
+
+//Passport configuration
+app.use(passport.initialize());
+require("./config/passport")(passport);
