bd0e1040e381717f88f897de87ea836e86538b9e
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-12-07 18:48:24 -0800

    v

--- client/src/components/Table/Newtable.js
+++ client/src/components/Table/Newtable.js
@@ -1,3 +1,5 @@
+import Axios from "axios";
+import { response } from "express";
 import React, { Component } from "react";
 import "./Table.css";
 class Newtable extends Component {
@@ -15,12 +17,13 @@ class Newtable extends Component {
         WORD_REPITITOR: "word_repititor",
         OTHER: "other",
       },
+      person: [],
     };
   }
-  setState = {
-    loading: true,
-    person: null,
-  };
+  //  setState = {
+  //     loading: true,
+  //     person: null
+  //   };
   renderTableHeader() {
     let header = Object.keys(this.state.fillerwords);
     return header.map((key, index) => {
@@ -28,22 +31,24 @@ class Newtable extends Component {
     });
   }
   renderTableData() {
-    let header = Object.keys(this.state.fillerwords);
-    return header.map((key, index) => {
-      return (
-       
-          <td key={index}>{key} </td>
-        
-      );
+    let body = Object.keys(this.state.person);
+    return body.map((key, index) => {
+      return <td key={index}>{key} </td>;
     });
   }
 
   async componentDidMount() {
-    const url = "http://localhost:8000/api/main/allparticipants";
-    const R = await fetch(url);
-    const DATA = await R.json();
-    console.log(DATA);
-    this.setState({ person: DATA, loading: false });
+    // const url = "http://localhost:8000/api/main/allparticipants";
+    // const R = await fetch(url);
+    // const DATA = await R.json();
+    // console.log(DATA);
+    // this.setstate.person = DATA;
+    //this.State({ person: DATA, loading: false });
+    Axios.get("http://localhost:8000/api/main/allparticipants")
+    .then(response => response.data)
+    .then((data)=> {this.setState({person: data});
+  });
+    
   }
   render() {
     return (
@@ -56,6 +61,9 @@ class Newtable extends Component {
               {this.renderTableHeader()}
             </tr>
             {this.renderTableData()}
+            <tr>
+            {this.state.person.length}
+            </tr>
           </tbody>
         </table>
       </div>
