c08b9b7c32b8376ccd8528995cfaa98d63c25a36
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-12-12 20:59:58 -0800

    n

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -42,7 +42,7 @@ class Login extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
       this.props.history.push("/table");
     }
